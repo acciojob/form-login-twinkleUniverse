4b67ad7b6bd7351e61f41ca8a1038ea7ea25f8b2
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-28 15:36:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getFormvalue() {
     //Write your code here
 	const inputRef= document.getElementsByTagName("input");
-	let firstName=inputRef[0].value;
+	let firstName=inputRef[0].value
 	let lastName=inputRef[1].value
-    alert(firstName+lastName);
+	let fullName=firstName+lastName;
+    alert(fullName);
 }
